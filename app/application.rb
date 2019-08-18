f893cd3bc87211d66fed612ee89e31d4b6f53b3a
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-08-18 18:01:08 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,34 @@
+#require 'pry'
+
+class Application 
+  
+def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+ 
+     if req.path.match(/items/)
+      item = @@items.find{|s| s.price}
+      resp.write "#{item.price}"
+     
+     elsif req.path=="/items/"
+        resp.write "Item not found"
+        resp.status = 400
+      
+     else req.path!="/items/"
+        resp.write "Route not found"
+        resp.status = 404 
+    end
+  
+  resp.finish
+  end
+  
+    if req.path.match(/songs/)
+ 
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
+ 
+      resp.write song.artist
+    end
+ 
+  
+end
